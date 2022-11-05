0cbd8f830756948aad1afe9e016b3bf22b6b8189
Ihor-Zhaivoron <105306196+Ihor-Zhaivoron@users.noreply.github.com>
2022-11-05 23:06:52 +0200

    get img...

---
+++ src/library.js
@@ -0,0 +1,8 @@
+// смена фона через джиес+подключить(Лучше через CSS все же)
+// отцентровать кнопки,фон изображение и границы контенера
+
+// повесить слушателя на input
+// функция которая будет запрашивать отдельно каждую кнопку
+const checkBntLibrary = document.querySelector('.storage__input');
+const bgrImgLibrary = document.querySelector('header');
+// bgrImgLibrary.style.backgroundImage = 'url(../images/header-bcg.jpg)';
