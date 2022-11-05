48f310a79f403366bb7f59acc6108d89fc73da87
Oleksandr Pavlov <sashapavlov20@gmail.com>
2022-11-05 20:56:04 +0200

    made FT-08 & FT-08

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import axios from 'axios';
 import Notiflix from 'notiflix';
 // import debounce from 'lodash.debounce';
 import galleryMarkup from '/src/templates/galleryMarkup.hbs';
+import { renderDefaultMovies } from './js/renderDefaultMovies';
 import oneMovieCard from '/src/templates/oneMovieCard.hbs';
 import refs from './js/refs';
 import fetchSearchFilm from './js/fetchAPI';
