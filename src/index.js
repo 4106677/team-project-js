2deab119761cff369339ff7cb108126fb9f866e7
Andrii Popov <popov.andrey80@gmail.com>
2022-11-05 20:31:26 +0200

    fix fetchAPI

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
-import axios from 'axios';
-import Notiflix from 'notiflix';
+// import axios from 'axios';
+// import Notiflix from 'notiflix';
 // import debounce from 'lodash.debounce';
-import galleryMarkup from '/src/templates/galleryMarkup.hbs';
-import oneMovieCard from '/src/templates/oneMovieCard.hbs';
-import refs from './js/refs';
-import fetchSearchFilm from './js/fetchAPI';
-import response from './js/onSubmitSearch';
+// import galleryMarkup from '/src/templates/galleryMarkup.hbs';
+// import oneMovieCard from '/src/templates/oneMovieCard.hbs';
+// import refs from './js/refs';
+// import fetchSearchFilm from './js/fetchAPI';
+// import onClickSubmit from './js/onSubmitSearch';
+
+// не удалять!!!
 import onClickSubmit from './js/onSubmitSearch';
+// console.log(response);
 
 // const refs = {
 //   form: document.querySelector('.search-form'),
