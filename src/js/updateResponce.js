ea1fb6f50c131c2620fd54e7c1f46734e3ebf9ca
Andrii Popov <popov.andrey80@gmail.com>
2022-11-06 17:42:40 +0200

    Update updateResponce.js

--- src/js/updateResponce.js
+++ src/js/updateResponce.js
@@ -3,13 +3,39 @@ import { onGetFilmGenres, onGetTVGenres } from './fetchAPI';
 
 function updateResponce(data) {
   console.log('updateResponce', data);
-  onGetFilmGenres().then(console.log);
+
+  const genresObj = {};
+  //   const genresObj2 = {
+  //     12: 'Adventure',
+  //     14: 'Fantasy',
+  //     16: 'Animation',
+  //   };
+
+  //   onGetFilmGenres().then(data => {
+  //     const dataGenres = data.data.genres;
+  //     dataGenres.forEach(item => {
+  //       genresObj[item.id] = item.name;
+  //     });
+  //   });
+  //   //   console.log('genresObj', genresObj);
+
+  //   onGetTVGenres().then(data => {
+  //     const dataGenres = data.data.genres;
+  //     dataGenres.forEach(item => {
+  //       genresObj[item.id] = item.name;
+  //     });
+  //   });
+  //   console.log('genresObj', genresObj);
+
+  // console.log(data.data.genres));
   //   console.log('onGetFilmGenres', onGetFilmGenres());
 
   //   const data = responce.data.results;
   //   console.log(data);
   //   const { poster_path, title, genre_ids, release_date, vote_average } = data;
   //   console.log(release_date);
+  const objIdGenres = getGenresId();
+  console.log('objIdGenres', objIdGenres);
 
   const newObj = data.map(item => {
     return {
@@ -18,10 +44,43 @@ function updateResponce(data) {
       title: item.title || item.name,
       vote: item.vote_average.toFixed(1),
       id: item.id,
+      genres: item.genre_ids.map(id => {
+        let genres = [];
+        if (Object.keys(objIdGenres).includes(id)) {
+          genres.push(Object.values(objIdGenres));
+        }
+        return id;
+      }),
     };
   });
   console.log('newObj', newObj);
   return newObj;
+}
+
+function getGenresId() {
+  const genresObj = {};
+  const genresObj2 = {
+    12: 'Adventure',
+    14: 'Fantasy',
+    16: 'Animation',
+  };
+
+  onGetFilmGenres().then(data => {
+    const dataGenres = data.data.genres;
+    dataGenres.forEach(item => {
+      genresObj[item.id] = item.name;
+    });
+  });
+  //   console.log('genresObj', genresObj);
+
+  onGetTVGenres().then(data => {
+    const dataGenres = data.data.genres;
+    dataGenres.forEach(item => {
+      genresObj[item.id] = item.name;
+    });
+  });
+  console.log('genresObj', genresObj);
+  return genresObj;
 }
 
 export default updateResponce;
