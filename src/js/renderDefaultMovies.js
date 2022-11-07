612a942b60240d53df1abab5b0fb0cf91e2b2683
Yaroslav Oliinyk <4106677@gmail.com>
2022-11-07 16:08:50 +0200

    добавляет отображение жанров и async запросов

--- src/js/renderDefaultMovies.js
+++ src/js/renderDefaultMovies.js
@@ -27,20 +27,20 @@ function fetchDefaultMoviesByApi() {
     .finally(() => spinnerOff());
 }
 
-export function renderDefaultMovies() {
-  fetchDefaultMoviesByApi().then(data => {
-    console.log('data.results', data.results);
-    const responce = updateResponce(data.results);
-    console.log('updateResponce', responce);
-    ul.insertAdjacentHTML('beforeend', oneMovieCardTpl(responce));
+export async function renderDefaultMovies() {
+  const data = await fetchDefaultMoviesByApi();
 
-    if (ul.childElementCount > 20) smoothScroll();
+  console.log('data.results', data.results);
+  const responce = await updateResponce(data.results);
+  console.log('updateResponce', responce);
+  ul.insertAdjacentHTML('beforeend', oneMovieCardTpl(responce));
 
-    if (data.page === 1000) {
-      alert("We're sorry, but you've reached the end of films collection.");
-      button.classList.add('is-hidden');
-    }
-  });
+  if (ul.childElementCount > 20) smoothScroll();
+
+  if (data.page === 1000) {
+    alert("We're sorry, but you've reached the end of films collection.");
+    button.classList.add('is-hidden');
+  }
 }
 
 renderDefaultMovies();
