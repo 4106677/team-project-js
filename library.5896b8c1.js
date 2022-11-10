2db4aa8313326c1d6f30390896bcd3568195b5d7
4106677 <104924953+4106677@users.noreply.github.com>
2022-11-10 17:21:28 +0000

    Deploying to gh-pages from @ 4106677/team-project-js@0178bf54e46d503de83c015b050da2fd0f129f98 🚀

---
+++ library.5896b8c1.js
@@ -0,0 +1,2 @@
+window.addEventListener("scroll",(function(){scrollY>1500?document.getElementById("btnUp").style.display="block":document.getElementById("btnUp").style.display="none"}));
+//# sourceMappingURL=library.5896b8c1.js.map
